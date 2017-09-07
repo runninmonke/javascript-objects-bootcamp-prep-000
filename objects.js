466b0566c266f834f1f993bf9ef9c4257461e51e
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-09-07 21:07:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 var playlist = {'Phil Ochs': "Here's to the State of Nebraska"};
+=======
+var playlist = {'Phil Ochs': "Here's to the State of Mississippi"};
+>>>>>>> 18057e67a9a9fa98f12f3bc0aadd61b75dc11b7f
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
